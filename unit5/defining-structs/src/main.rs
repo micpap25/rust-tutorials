72871ad06c867384fae7b530f1befe6ae9e2b43e
micpap25 <micpap25@gmail.com>
2021-05-20 23:24:18 -0400

    5.1 Defining and Instantiating Structs

--- unit5/defining-structs/src/main.rs
+++ unit5/defining-structs/src/main.rs
@@ -10,12 +10,20 @@ struct User {
     active: bool,
 }
 
+// Tuple structs are ways to create organized structures without naming individual fields
+
+struct Color(i32, i32, i32);
+
+// Lastly, unit structs are useful for type implementation
+
+struct Unit();
+
 fn main() {
     // After defining a struct, you can create instances of it in your code.
     // Each field is given data in a key: value format.
     // The fields do not have to be assigned in the order they were defined.
 
-    let _user1 = User {
+    let user1 = User {
         // As a reminder, presenting different ways to go 
         // from &str (pointer to string data in program) to String (heap-allocated string)
         username: String::from("micpap25"), 
@@ -24,6 +32,7 @@ fn main() {
         sign_in_count: 37
     };
 
+    println!("{} {}", user1.username, user1.email);
     // If the struct instance is mutable, you can also use dot notation to edit the values
     // You cannot make specific fields mutable; the whole instance is mutable or immutable
 
@@ -40,6 +49,39 @@ fn main() {
 
     let _user3 = build_user_bad("dude@gmail.com".to_string(), "dude".to_string());
     let _user4 = build_user_good("guy@gmail.com".to_string(), "dude".to_string());
+
+    // We can use attributes from other instances to build a new instance
+
+    let _user5 = User {
+        username: String::from("user"),
+        email: String::from("user@gmail.com"),
+        active: user1.active,
+        sign_in_count: user1.sign_in_count
+    };
+
+    // There's a shorthand way to do this, too
+    // ..<instance name> fills in all unspecified values with the instance's values.
+
+    let _user6 = User {
+        active: true,
+        sign_in_count: 2,
+        ..user1
+    };
+
+    // Instantiate a tuple struct as so:
+
+    let _black = Color(0, 0, 0);
+
+    // And a unit struct:
+
+    let _unit = Unit();
+
+    // When defining a struct, it is simplest to use an owned type 
+    // like String, rather than a referenced type like &str. 
+    // It makes sense that the struct owns all its data. 
+    // You could make a struct with references, 
+    // but that requires using lifetimes (which we'll learn about later)
+    // to assure the data's existence.
 
 }
 
